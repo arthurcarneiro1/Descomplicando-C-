79164765c58074866a512ca6cac3c2fba3f4256c
Arthur Opirata <arthurcarneiro098@gmail.com>
2025-04-10 14:23:40 -0300

    feat: implementando Aula3

--- Aula2TipoDeDados/obj/Debug/net9.0/Aula2TipoDeDados.AssemblyInfo.cs
+++ Aula2TipoDeDados/obj/Debug/net9.0/Aula2TipoDeDados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ab9e11a637994477a3a9f94573d3e17576b8a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbe069c2dc968c7b566fc44edbe0bf8cfa7d577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula3OperadoresAtribuicao/Program.cs
@@ -0,0 +1,35 @@
+﻿// Operadores de Atribuição
+
+//para atribuir um valor usamos o 
+//=
+
+using System.IO.Pipelines;
+
+string nome = "arthur";
+nome="Sam"; //valor mudou
+
+//encremento
+//+=
+int contador = 10;
+contador +=2; //12 , encremento mais 2 
+
+
+//decremento
+//-=
+contador-=2; // 10 . diminuo o valor
+
+// converte valores 
+// parse
+
+string numero = "3";
+
+int n = int.Parse(numero); // numero convertido para string
+
+//conversão segura
+int.TryParse(numero, out int result); // ele converte e senão conseguir retorna null
+Console.WriteLine(result);
+
+// conversão implicita com cast
+
+double n2 = 2.4;
+int n4 = (int)n2; // usado para converter 
