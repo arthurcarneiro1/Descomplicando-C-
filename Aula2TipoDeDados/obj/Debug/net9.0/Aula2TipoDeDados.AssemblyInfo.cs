f7c3aed374c73da69147af543927542004bf640c
Arthur Opirata <arthurcarneiro098@gmail.com>
2025-04-12 10:27:05 -0300

    feat: implentação Aula4

--- Aula2TipoDeDados/obj/Debug/net9.0/Aula2TipoDeDados.AssemblyInfo.cs
+++ Aula2TipoDeDados/obj/Debug/net9.0/Aula2TipoDeDados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbe069c2dc968c7b566fc44edbe0bf8cfa7d577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79164765c58074866a512ca6cac3c2fba3f4256c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula2TipoDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula4CondiciaisEDebug/Program.cs
@@ -0,0 +1,47 @@
+﻿//Estrutura condicial 
+//usada para ser executado caso a condição seja verdadeira
+
+using System.Reflection.Metadata;
+
+// if (condiçao){
+//     bolco executado
+// }else{
+// e executado caso seja falso
+//}
+//
+
+if(10>20){
+    Console.WriteLine("if executado");
+}else if(10>3){
+    Console.WriteLine("else if executado");
+}else{
+Console.WriteLine("else executado");
+}
+
+
+//Debug
+// mostrar o codigo linha por linha
+//comando Debug
+// breakpoint:onde o codigo para  vc marca com o mause
+//f5 executa o debug
+//f10 avança
+
+
+// switch case
+// uma alternativa para if e else
+int n = 2;
+switch (n)
+{
+    case 1:
+    Console.WriteLine("1");
+    break;
+    case 2:
+    Console.WriteLine("2");
+    break;
+    case 3:
+    Console.WriteLine("3");
+    break;
+    default:
+        // Código a ser executado se nenhum dos casos for atendido
+        break;
+}
