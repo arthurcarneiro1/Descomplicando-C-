9339c22d5f783802e9d847cefd66cbdd3f1c448c
Arthur Opirata <arthurcarneiro098@gmail.com>
2025-04-12 10:37:17 -0300

    feat: implementando Aula5

--- Aula5OperadoresAritimeticos/Program.cs
+++ Aula5OperadoresAritimeticos/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Operadores aritimeticos
+
+int n1= 0;
+//+ : adição e concatena~]ao
+n1=2+2;
+//- : subtração
+n1=2-2;
+// * : multiplicação
+n1 = 2*2; 
+// / : divisão
+n1 = 2 /2;
+// % : resto de divisão
+n1= 4%2;
+
+// encremento e decrementeo rapido
+//++ : encremento
+n1++;
+//-- : decremento
+n1--;
+
+//OBJ MATH
+// possue varias funçoes que ajudam a realizar calculos
+
+//raiz
+Math.Sqrt(n1);
+
+//potencia
+Math.Pow(n1,2);
+
+//trigonometria
+Math.Tan(n1); // tangente
+Math.Cos(n1); // cosseno
+Math.Sin(n1); //seno
