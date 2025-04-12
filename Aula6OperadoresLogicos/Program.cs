12d52bd6fefc08ea85613e0dffba6a9fc6cb1862
Arthur Opirata <arthurcarneiro098@gmail.com>
2025-04-12 10:48:52 -0300

    feat: implementado Aula6

---
+++ Aula6OperadoresLogicos/Program.cs
@@ -0,0 +1,23 @@
+﻿//Operadores logicos
+
+//&& é
+//todos precisam ser verdadeiro
+//ex:
+using System.Reflection.Metadata;
+
+bool notaSuficiente = true;
+bool frequencia= true;
+bool result = notaSuficiente  && frequencia; // as duas condiçoes precisam atender para retornar True
+
+//|| ou
+// so apenas 1 precia ser verdadeiro
+bool foneEsquerdo = true;
+bool foneDireito= false;
+bool ouvirMusica= foneEsquerdo || foneDireito; // como uma condiçao foi verdadeira  o resulado e true
+//!  not
+// negaçao logica inverte o resultado 
+bool chuva = false;
+if (!chuva)// mudou o resultado para true
+{
+    Console.Write("esta chovendo");
+}
